e031ac1b1096e5bdeee3fe00e433792e641b7deb
Imran Hossen <freelancerimran81@gmail.com>
2022-05-14 00:58:28 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
 import SignUp from './Pages/Login/SignUp';
 import RequireAuth from './Pages/Login/RequireAuth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
           </RequireAuth>
         } />
       </Routes>
+      <ToastContainer/>
     </div>
   );
 }
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,16 +2,49 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
-    const [user, loading] = useAuthState(auth);
+    const [user] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
 
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id, name, slot);
-        setTreatment(null);
+
+        const booking = {
+            treatmentId: _id,
+            treatment: name,
+            date:formattedDate,
+            slot,
+            patient: user.email,
+            patientName: user.displayName,
+            phone: event.target.phone.value
+        }
+
+        fetch('http://localhost:5000/booking', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // to close the modal
+                console.log(data);
+                if (data.success) {
+                    toast(`Appointment is set, ${formattedDate} at ${slot}`)
+
+                }
+                else {
+                    toast.error(`Already have an Appointment on, ${data.booking?.date} at ${data.booking?.slot}`)
+                }
+                setTreatment(null);
+            })
+
+
     }
 
     return (
@@ -40,7 +73,7 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                             }
                         </select>
 
-                        <input type="phone" name='phone' placeholder="Enter Phone Number" className="input input-bordered w-full my-2 focus:outline-none" />
+                        <input type="phone" name='phone' placeholder="Enter Phone Number" className="input input-bordered w-full my-2 focus:outline-none" required />
 
                         <input type="submit" value="Submit" className="btn btn-primary bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary w-full my-2 " />
                     </form>
