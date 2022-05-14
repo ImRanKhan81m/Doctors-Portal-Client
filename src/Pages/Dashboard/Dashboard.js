ad8ffed1182bf9ac2b50961d7744f03e736ba729
Imran Hossen <freelancerimran81@gmail.com>
2022-05-14 22:35:25 +0600

    dashboard data setup done

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,13 +4,13 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile mid-content">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle " />
-            <div class="drawer-content">
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle " />
+            <div class="drawer-content lg:px-8 md:px-8 sm:px-7 px-4 pt-5">
                 <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
-                 <Outlet></Outlet>
-            </div> 
+                <Outlet></Outlet>
+            </div>
             <div class="drawer-side ">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to="/dashboard">Sidebar Item 1</Link></li>
